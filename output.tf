a99422111deb818364fd09e69217ce0be87cde44
mtahir <mo.tahir@contino.io>
2017-08-17 23:05:31 +0100

    added output variables

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "host_name" {
   value = "${azurerm_template_deployment.postgres-paas.name}.postgres.database.azure.com"
 }
+
+output "postgresql_listen_port" {
+  value = "${var.postgresql_listen_port}"
+}
+
+output "postgresql_database" {
+  value = "${var.postgresql_database}"
+}
