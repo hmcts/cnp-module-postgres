1bf771b2c81455b8909b30094bf191b6f6220a14
sanabriad <sanabria.d@gmail.com>
2017-08-29 13:53:17 +0100

    Adding control to check postgresql connection
    Checking connection

---
+++ tests/int/test/integration/default/controls/postgresql_conn.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+# copyright: 2017, The Authors
+# license: All rights reserved
+
+title 'Check Azure Postgresql connection'
+
+control 'azure-postgresql-conn' do
+
+  impact 1.0
+  title ' Check that we can connect to postgresql'
+  json_obj = json('.kitchen/kitchen-terraform/default-azure/terraform.tfstate')
+  random_name = json_obj['modules'][0]['outputs']['random_name']['value'] + '-db-data-int'
+  # Create a PostgreSQL session:
+  sql = postgres_session('inspec', '0Tk304$9&@6k', random_name + "postgres.database.azure.com")
+
+  describe sql.query('SELECT * FROM postgres') do
+    its('output') { should eq '' }
+  end
+end
