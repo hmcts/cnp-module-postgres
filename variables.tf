168caf603ee13d01f2608588098de86cbd961090
Liam F. O'Neill <liamfoneill@outlook.com>
2018-05-31 16:56:55 +0100

    Updated Variables to new API

--- variables.tf
+++ variables.tf
@@ -22,29 +22,36 @@ variable "resource_group_name" {
 
 //POSTGRES MODULE SPECIFIC VARIABLES
 variable "postgres_server_name" {
-  type = "string"
+  type        = "string"
   description = "The name of your PostgreSQL Server"
 }
 
 variable "postgresql_database_name" {
-  type = "string"
+  type        = "string"
   description = "The name of your PostgreSQL Database"
 }
+
 variable "postgresql_server_sku_name" {
   type        = "string"
-  description = "(Optional) Specifies the SKU Name for this PostgreSQL Server. Possible values are: PGSQLB50, PGSQLB100, PGSQLS100, PGSQLS200, PGSQLS400 and PGSQLS800"
-  default     = "PGSQLB50"
+  description = "(Required) Specifies the SKU Name for this PostgreSQL Server. The name of the SKU, follows the tier + family + cores pattern (e.g. B_Gen4_1, GP_Gen5_8). For more information see the product documentation."
+  default     = "B_Gen5_2"
+}
+
+variable "postgresql_server_sku_family" {
+  type        = "string"
+  description = "(Required) The family of hardware Gen4 or Gen5, before selecting your family check the product documentation for availability in your region."
+  default     = "Gen5"
 }
 
 variable "postgresql_server_sku_capacity" {
-  description = "(Optional) Specifies the DTU's for this PostgreSQL Server. Possible values are 50 and 100 DTU's when using a Basic SKU and 100, 200, 400 or 800 when using the Standard SKU."
-  default     = 100
+  description = "((Required) The scale up/out capacity, representing server's compute units."
+  default     = 2
 }
 
 variable "postgresql_server_sku_tier" {
   type        = "string"
-  description = "(Optional) Specifies the SKU Tier for this PostgreSQL Server. Possible values are Basic and Standard."
-  default     = "Standard"
+  description = "(Required) The tier of the particular SKU. Possible values are Basic, GeneralPurpose, and MemoryOptimized. For more information see the product documentation."
+  default     = "GeneralPurpose"
 }
 
 variable "administrator_login" {
@@ -60,8 +67,8 @@ variable "version" {
 
 variable "storage_mb" {
   type        = "string"
-  description = "(Required) Specifies the amount of storage for the PostgreSQL Server in Megabytes. Possible values are shown below. Changing this forces a new resource to be created.Possible values for storage_mb when using a SKU Name of Basic are: - 51200 (50GB) - 179200 (175GB) - 307200 (300GB) - 435200 (425GB) - 563200 (550GB) - 691200 (675GB) - 819200 (800GB) - 947200 (925GB) Possible values for storage_mb when using a SKU Name of Standard are: - 128000 (125GB) - 256000 (256GB) - 384000 (384GB) - 512000 (512GB) - 640000 (640GB) - 768000 (768GB) - 896000 (896GB) - 1024000 (1TB)"
-  default     = "128000"
+  description = "(Required) Max storage allowed for a server, possible values are between 5120 MB (5GB) and 1048576 MB (1TB). The step for this value must be in 1024 MB (1GB) increments. For more information see the product documentation."
+  default     = "5120"
 }
 
 variable "ssl_enforcement" {
