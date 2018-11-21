56cc4431c4198dd39bd426e9331e2ee13867f408
Constantin Preda <constantin.preda@hmcts.net>
2018-11-21 12:36:07 +0000

    vriable removed

--- main.tf
+++ main.tf
@@ -13,8 +13,6 @@ locals {
   jenkins_subnet_id       = "/subscriptions/${local.jenkins_subscription_id}/resourceGroups/${local.jenkins_rg}/providers/Microsoft.Network/virtualNetworks/${local.jenkins_vnet}subnets/${var.jenkins_subnet_name}"
   bastion_subnet_id       = "/subscriptions/${local.bastion_subscription_id}/resourceGroups/${local.bastion_rg}/providers/Microsoft.Network/virtualNetworks/${local.bastion_vnet}/subnets/${local.bastion_subnet_name}"
   ase_subnet_id           = "${data.azurerm_subnet.ase.id}"
-
-  #ase_subnet_id           = "/subscriptions/${data.azurerm_subscription.current.id}/resourceGroups/core-infra-${var.env}/providers/Microsoft.Network/virtualNetworks/core-infra-vnet-${var.env}/subnets/core-infra-subnet-3-${var.env}"
   ase_vnet_rule_name      = "${var.env}ASEVNET"
   bastion_vnet_rule_name  = "${var.env}BastionVNET"
   jenkins_vnet_rule_name  = "${var.env}JenkinsVNET"
