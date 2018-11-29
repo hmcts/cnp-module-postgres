3bfdf637fb69a3db96b7b3c3995251618ada4f3b
Constantin Preda <constantin.preda@hmcts.net>
2018-11-29 15:44:28 +0000

    changes

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ locals {
   bastion_vnet            = "${(var.env == "prod") ? (var.env == "aat" ) ? "betaProdVNet" : "betaPreProdVNet" : "reformMgmtCoreVNet"}"
   bastion_subnet_name     = "${(var.env == "prod") ? (var.env == "aat" ) ? "betaProdDataSN" : "betaPreProdDataSN" : "reformMgmtDmzSN"}"
   vaultname               = "${(var.env == "prod") ? (var.env == "aat" || var.env == "demo" || var.env == "preview") ? (var.env == "hmcts-demo") ? "infra-vault-prod" : "infra-vault-nonprod" : "infra-vault-hmctsdemo" : "infra-vault-sandbox"}"
-  core_infra_subnet_no    = "${(var.env == "idam") ? "2" : "3"}"
+  core_infra_subnet_no    = "${(var.env != "idam") ? "3" : "2"}"
   ase_subnet_id           = "${data.azurerm_subnet.ase.id}"
-  #asev2_subnet_id         = "${var.asev2_presence == "present" ? data.azurerm_subnet.asev2.id : "null"}"
+  asev2_subnet_id         = "${var.asev2_presence == "present" ? data.azurerm_subnet.asev2.id : "null"}"
   jenkins_subnet_id       = "/subscriptions/${local.jenkins_subscription_id}/resourceGroups/${local.jenkins_rg}/providers/Microsoft.Network/virtualNetworks/${local.jenkins_vnet}/subnets/${var.jenkins_subnet_name}"
   bastion_subnet_id       = "/subscriptions/${local.bastion_subscription_id}/resourceGroups/${local.bastion_rg}/providers/Microsoft.Network/virtualNetworks/${local.bastion_vnet}/subnets/${local.bastion_subnet_name}"
 
@@ -51,6 +51,9 @@ data "azurerm_subnet" "asev2" {
   name                 = "core-infra-subnet-ase-${var.env}v2"
   virtual_network_name = "core-infra-vnet-${var.env}v2"
   resource_group_name  = "core-infra-${var.env}v2"
+}
+output "asev2_subnet_id" {
+  value = "${element(concat(data.azurerm_subnet.asev2.*.id, list("")), 0)}"
 }
 
 data "azurerm_key_vault_secret" "DCD-CNP-Prod-subscription-id" {
@@ -98,7 +101,7 @@ resource "azurerm_template_deployment" "postgres-paas" {
     AseVnetRuleName            = "${local.ase_vnet_rule_name}"
     AseSubnetId                = "${local.ase_subnet_id}"
     Asev2VnetRuleName          = "${local.asev2_vnet_rule_name}"
-    #Asev2SubnetId              = "${local.asev2_subnet_id}"
+    Asev2SubnetId              = "${local.asev2_subnet_id}"
     AseV2Presence              = "${var.asev2_presence}"
     BastionVnetRuleName        = "${local.bastion_vnet_rule_name}"
     BastionSubnetId            = "${local.bastion_subnet_id}"
