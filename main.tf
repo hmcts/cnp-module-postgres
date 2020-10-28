c5bf68c5ac389a8047fc7fe1d2781da1bda712f0
Ciarán Carragher <c.carragher@kainos.com>
2020-10-28 14:15:17 +0000

    Edit for_each argument for testing

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_postgresql_database" "postgres-db" {
 }
 
 resource "azurerm_postgresql_virtual_network_rule" "postgres-vnet-rule" {
-  for_each                             = local.dbrulesnew
+  for_each                             = "${local.dbrulesnew}"
   name                                 = each.value.rule_name
   resource_group_name                  = "${azurerm_resource_group.data-resourcegroup.name}"
   server_name                          = "${var.product}-${var.env}"
