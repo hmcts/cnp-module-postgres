1c45870bf644aeaf362daf6b1772ade03dd04c4a
Matt Slater <matt.slater@hmcts.net>
2020-11-25 15:21:22 +0000

    Changing variable subsitution method as it's not working in the pipeline

--- find-subnets.py
+++ find-subnets.py
@@ -49,7 +49,7 @@ query = json.loads(line)
 subnets_filename = query['subnets_filename']
 github_token = query['github_token']
 
-url = f'https://raw.githubusercontent.com/hmcts/cnp-database-subnet-whitelisting/master/{subnets_filename}'
+url = 'https://raw.githubusercontent.com/hmcts/cnp-database-subnet-whitelisting/master/%s' % subnets_filename
 
 req = urllib.request.Request(
     url=url, headers={'Authorization': 'Bearer ' + github_token})
