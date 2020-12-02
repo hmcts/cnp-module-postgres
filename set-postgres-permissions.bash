10c9f0ce3dfed0cd7599e68f00dc4c6415945270
Tim Jacomb <tim.jacomb@hmcts.net>
2020-12-02 16:55:33 +0000

    Tweaks to script to make it work with postgres 10

--- set-postgres-permissions.bash
+++ set-postgres-permissions.bash
@@ -6,9 +6,9 @@ az login -u "${DB_MANAGER_USER_NAME}" -p "${DB_MANAGER_PASSWORD}" -t "${TENANT_I
 export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query accessToken -o tsv)
 
 SQL_COMMAND="
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO PUBLIC;
-DROP ROLE IF EXISTS \"${DB_READER_USER}\";
 CREATE ROLE \"${DB_READER_USER}\" WITH LOGIN IN ROLE azure_ad_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO PUBLIC;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"${DB_READER_USER}\";
 "
 
 psql "sslmode=require host=${DB_HOST_NAME} dbname=${DB_NAME} user=${DB_USER}" -c "${SQL_COMMAND}"
