da3cbc77bb4136cd6cb3802f1bdf87e556796778
Thomas Thornton <>
2021-02-15 12:11:08 +0000

    no message

--- private-endpoint.tf
+++ private-endpoint.tf
@@ -17,15 +17,3 @@ resource "azurerm_private_endpoint" "postgres" {
   }
   count = var.subnet_id == "" ? 0 : 1
 }
-
-# resource "azurerm_private_dns_a_record" "postgres" {
-#   provider = azurerm.private_dns
-
-#   name                = azurerm_postgresql_server.postgres-paas.name
-#   zone_name           = "privatelink.postgres.database.azure.com"
-#   resource_group_name = "core-infra-intsvc-rg"
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.postgres[0].private_service_connection[0].private_ip_address]
-
-#   count = var.subnet_id == "" ? 0 : 1
-# }
