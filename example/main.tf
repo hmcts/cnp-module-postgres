74c7494fc76ac4c443b05875e20b1da8f106b0f8
Thomas Thornton <>
2022-08-22 11:35:47 +0100

    update test

--- example/main.tf
+++ example/main.tf
@@ -2,6 +2,7 @@ module "db" {
   source             = "../"
   common_tags        = var.common_tags
   database_name      = "test"
+  database_name2     = "test2"
   env                = var.env
   location           = var.location
   postgresql_user    = "test"
